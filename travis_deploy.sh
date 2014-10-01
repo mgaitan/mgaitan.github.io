e1e18b6b3dfe53b8db854858ced63a8a8766a331
Martín Gaitán <gaitan@gmail.com>
2014-09-30 22:50:33 -0300

    Evidence: I dont what I'm doing. take 2

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -15,6 +15,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git add .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
     git push -fq $TARGET_REPO $SOURCE_BRANCH > /dev/null
-    git push -fq TARGET_REPO `git subtree split --prefix=$NIKOLA_OUTPUT_FOLDER $GH_BRANCH`:$GH_BRANCH
+    git push -fq $TARGET_REPO `git subtree split --prefix=$NIKOLA_OUTPUT_FOLDER $GH_BRANCH`:$GH_BRANCH
     echo -e "Deploy completed\n"
 fi
