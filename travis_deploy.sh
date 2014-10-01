dcd9eb79043b0fe39c8e6ce1d5d365972609454e
Martín Gaitán <gaitan@gmail.com>
2014-09-30 21:20:48 -0300

    force push?

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -11,9 +11,10 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "Travis"
     fi
+    nikola build
     git add .
     git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push $SOURCE_BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO
+    git push -fq https://${GH_TOKEN}@github.com/$TARGET_REPO $SOURCE_BRANCH > /dev/null
     git subtree push --prefix=$NIKOLA_OUTPUT_FOLDER https://${GH_TOKEN}@github.com/$TARGET_REPO $GH_BRANCH
     echo -e "Deploy completed\n"
 fi
